f2ea5ca92beba43b92309b6d603dd908b8213ec0
Girish Somashekar <srbindas70@gmail.com>
2021-05-15 13:37:55 -0400

    adding files from May 15th

--- data.js
+++ data.js
@@ -35,7 +35,7 @@ function buildingredienttable() {
                     var chkbox = cell.append("input");
                     chkbox.attr("value", value);
                     chkbox.attr("type", "checkbox");
-                    chkbox.attr("id", value);
+                    chkbox.attr("id", value.replace("'", "").replace("*", "").replace("&", "").replaceAll(" ", "_"));
                     var label = cell.append("label");
                     label.attr("for", value);
                     label.text(value);
@@ -75,125 +75,50 @@ var finalingredients = [];
 
 function user_selections(){
 
-    var user_selection = [];
+    var user_selection = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
     d3.json("data/just_ingredients.json").then((data) => { 
-        var data = data;
-        // console.log(data);
-        // return data;
+        var data = data["Ingredients"];
 
-        // var i;
-        // for (i = 0; i < data.length; i++) {
-        //     // text += cars[i] + "<br>";
-        //     console.log(i);
-        //     finalingredients.push(i);
-        // }  
-
-        // data.forEach(item => {
+        console.log(data);
         Object.entries(data).forEach(([key, value]) => {
-            var i = value;
-            finalingredients.push(i);
-        }
-        )
+            console.log(d3.select("#" + value.replace("'", "").replace("*", "").replace("&", "").replaceAll(" ", "_")))
+            // console.log(d3.select)
+            if (d3.select("#" + value.replace("'", "").replace("*", "").replace("&", "").replaceAll(" ", "_")).property("checked")){
+                console.log(key)
+                user_selection[key] = 1
+            }
+        });
 
-    console.log(finalingredients);   
+        console.log(user_selection);
+        d3.json('http://127.0.0.1:5000/model', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user_selection)
+        }).then(data => {
+            console.log("Result: " + data.result)
+        });
+
+    //     for (i = 0; i < data.length; i++) {
+    //         console.log(data[i])
+    //         if (d3.select("#(data[i])").checked);
+    //             user_selection[data[i]] = 1
+    // }
+    })
+    // + ' input[type="checkbox"]:checked
+   
+    // d3.json('http://127.0.0.1:5000/model', function(error, data){
+    //     console.log(data)
+    // })
+    // .header("Content-Type", "application/json")
+    // .send("POST", JSON.stringify({"data": user_selection}));
+        
+    // console.log(finalingredients);   
 
         }
-        )}
-     
  
-// function buildGraph() {
-//     d3.json("http://127.0.0.1:5000/ingredient_count").then((data) => {
-//         console.log(data)
-//         var graphData = data;
-//         //Create lists to collect data to graph.
-//         var ingredients = [];
-//         var count = [];
-       
-        //Get data for the plots
-        // for (var i = 0; i < data.length; i++) {
-        //     if (graphData[i]..split(" ")[1]) {
-        //         cost.push(graphData[i].budget.split(" ")[1]);
-        //     }else{cost.push(0)}
-        //     if (graphData[i].worldwide_gross_income.split(" ")[1]){
-        //         revenue.push(graphData[i].worldwide_gross_income.split(" ")[1]);
-        //     }else{revenue.push(0)}
-        //     labels.push(graphData[i].title);
-        // }
-        // console.log(cost);
-        // console.log(revenue);
-        // console.log(labels);
-        // var trace1 = {
-        //     x: labels,
-        //     y: cost,
-        //     type: 'bar',
-        //     name: 'Cost',
-        //     width: 0.5,
-        //     marker: {
-        //         color: 'red'
-        //     }
-        // }
-        // var trace2 = {
-        //     x: labels,
-        //     y: revenue,
-        //     type: 'bar',
-        //     name: 'Revenue',
-        //     width: 1.0,
-        //     marker: {
-        //         opacity: 0.5,
-        //         color: 'green'
-        //     }
-        // }
-        // var barLayout = {
-        //     title: 'Financial Performance',
-        //     font:{family:'Arial bold',size:12,backgroundcolor:'#867E7E'},            
-        //     xaxis: {
-        //         tickangle: -45,
-        //     },
-        //     barmode: 'overlay'
-        // };
-        // var barData = [trace2, trace1];
-        // Plotly.newPlot("bar", barData, barLayout);
-//     }
-//     )
-// }
-
-
-// function optionChanged(filter_type, value) {
-//     //Check the value of each options
-//     var c_dropdown = d3.select("#selcountryDataset");
-//     var g_dropdown = d3.select("#selgenreDataset");
-//     var startyear_dropdown = d3.select("#selstartyearDataset");
-//     var endyear_dropdown = d3.select("#selendyearDataset");
-//     query_string = "?"
-//     //if value exists, build query string
-//     if (c_dropdown.property("value") && c_dropdown.property("value") != "All") {
-//         country = c_dropdown.property("value");
-//         query_string = query_string + "country=" + country + "&"
-//     }
-//     if (g_dropdown.property("value") && g_dropdown.property("value") != "All") {
-//         genre = g_dropdown.property("value");
-//         query_string = query_string + "genre=" + genre + "&"
-//     }
-//     if (startyear_dropdown.property("value")) {
-//         startyear = startyear_dropdown.property("value");
-//         query_string = query_string + "startyear=" + startyear + "&"
-//     }
-//     if (endyear_dropdown.property("value")) {
-//         endyear = endyear_dropdown.property("value");
-//         query_string = query_string + "endyear=" + endyear
-//     }
-//     // query_string="?"
-//     // if country:
-//     // query_string = query_string + "country=" + country + "&"
-//     // if genre:
-//     //query_string = query_string + "genre=" + genre + "&" 
-//     // buildingredienttable(query_string);
-//     // buildGraph(query_string);
-//     // buildMap(query_string);
-//     // getDemoInfo(id);
-// }
-
 function init() {
 
     
